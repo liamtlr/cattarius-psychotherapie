7e725e2d73315ba0b16fc41c3ef46404289b1905
Liam Taylor <liam@piclo.energy>
2023-05-01 16:43:37 +0100

    add footer link content

---
+++ pages/datenschutz.vue
@@ -0,0 +1,26 @@
+<template>
+  <content-wrapper title="Datenschutz">
+    <h2>Title</h2>
+    <p>
+      Lorem ipsum...
+    </p>
+    <p>
+      Lorem ipsum...
+    </p>
+    <p>
+      Lorem ipsum...
+    </p>
+  </content-wrapper>
+</template>
+
+<script setup>
+  useHead({
+    title: 'Datenschutz',
+    meta: [
+      {
+        name: 'description',
+        content: 'Wie Ihr Daten beschutz wird',
+      },
+    ],
+  })
+</script>
---
+++ pages/impressum.vue
@@ -0,0 +1,26 @@
+<template>
+  <content-wrapper title="Impressum">
+    <h2>Title</h2>
+    <p>
+      Lorem ipsum...
+    </p>
+    <p>
+      Lorem ipsum...
+    </p>
+    <p>
+      Lorem ipsum...
+    </p>
+  </content-wrapper>
+</template>
+
+<script setup>
+  useHead({
+    title: 'Impressum',
+    meta: [
+      {
+        name: 'description',
+        content: 'Untershiedlicher legale Sachen',
+      },
+    ],
+  })
+</script>
