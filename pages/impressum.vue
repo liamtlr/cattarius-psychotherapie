902efaf41eb3146ab0c049a82a596a5d7e714a24
Liam Taylor <liam@piclo.energy>
2023-05-06 16:26:54 +0100

    dd impressum copy

--- pages/impressum.vue
+++ pages/impressum.vue
@@ -1,14 +1,189 @@
 <template>
   <content-wrapper title="Impressum">
-    <h2>Title</h2>
     <p>
-      Lorem ipsum...
+      Pflichtangaben gemäß § 5 Telemediengesetz (TMG) und § 55 Rundfunk
+      Staatsvertrag (RSTV)<br>
+      Name und Anschrift des Psychotherapeuten<br>
+      M.Sc. Marcel Cattarius - Praxis für Psychotherapie<br>
+      Martinsstraße 17<br>
+      55116 Mainz<br>
+      Telefon: +49 6131 – 945 25 70<br>
+      E-Mail: info[at]psychotherapiecattarius.de<br>
+      Internet:
+      <a href="https://www.psychotherapiecattarius.de">
+        www.psychotherapiecattarius.de
+      </a><br>
     </p>
     <p>
-      Lorem ipsum...
+      Berufsbezeichnung<br>
+      Psychologischer Psychotherapeut, verliehen in der Bundesrepublik
+      Deutschland durch das Landesamt für Gesundheit und Soziales von
+      Rheinland-Pfalz
     </p>
     <p>
-      Lorem ipsum...
+      Berufshaftpflichtversicherung<br>
+      Barmenia Versicherungen<br>
+      Barmenia-Allee 1<br>
+      42119 Wuppertal<br>
+    </p>
+    <p>
+      Telefon: 0202 438-00<br>
+      Fax: 0202 438-2703<br>
+      info@barmenia.de<br>
+    </p>
+    <p>
+      Angaben zum Diensteanbieter (verantwortlich im Sinne des mediendienste
+      Staatsvertrages, §6, Abs. 2)<br>
+      Marcel Cattarius<br>
+      Martinsstraße 17<br>
+      55116 Mainz<br>
+      Telefon: +49 6131 – 945 25 70<br>
+      E-Mail: info[at]psychotherapiecattarius.de<br>
+      Internet: www.psychotherapiecattarius.de<br>
+    </p>
+    <p>
+      Praxismarketing | Layout, Realisierung, Betreuung<br>
+      Marcel Cattarius<br>
+    </p>
+    <p>
+      Copyright Fotos<br>
+      Marcel Cattarius<br>
+    </p>
+    <h2>Zuständige Aufsichtsbehörden</h2>
+    <p>
+      1. Ministerium für Gesundheit des Landes Rheinland-Pfalz
+      Ministerium für Soziales, Arbeit, Gesundheit und Demografie des Landes
+      Rheinland-Pfalz<br>
+      Bauhofstr. 9<br>
+      55116 Mainz<br>
+      Telefon: +49 6131 – 16 20 27<br>
+      Fax: +49 6131 – 16 24 52<br>
+      E-Mail: poststelle(at)msagd.rlp.de<br>
+    </p>
+    <p>
+      2. Anschrift der kassenärztlichen Vereinigung Rheinland-Pfalz als die
+      für die Vertragsärzte zuständige Aufsichtsbehörde<br>
+      Kassenärztliche Vereinigung Rheinland-Pfalz (KV RLP)<br>
+      Isaac-Fulda-Allee 14<br>
+      55124 Mainz<br>
+      Telefon: +49 6131 – 32 63 26<br>
+      Fax: +49 6131 – 32 63 27<br>
+      E-Mail: service@kv-rlp.de<br>
+      Internet: <a href="kv-rlp.de">kv-rlp.de</a><br>
+    </p>
+    <p>
+      3. Anschrift der zuständigen Psychotherapeutenkammer als Aufsichtsbehörde
+      Landespsychotherapeutenkammer Rheinland-Pfalz<br>
+      Körperschaft des öffentlichen Rechts<br>
+      Diether-von-Isenburg-Straße 9 – 11<br>
+      55116 Mainz<br>
+      Telefon: +49 6131 – 93 05 50<br>
+      Fax: +49 6131 – 93 05 52<br>
+      E-Mail: service(at)lpk-rlp.de<br>
+      Internet: <a href="lpk-rlp.de">lpk-rlp.de</a><br>
+    </p>
+    <h2>Zuständige Aufsichtsbehörden</h2>
+    <p>
+      1. Berufsordnung der Kammer für Psychologische Psychotherapeuten und
+      Kinder- und Jugendlichenpsychotherapeuten in Rheinland-Pfalz<br>
+      <a href="https://www.lpk-rlp.de/fileadmin/pdf-downloads/BO_LPK_RLP.pdf">
+        https://www.lpk-rlp.de/fileadmin/pdf-downloads/BO_LPK_RLP.pdf
+      </a><br>
+    </p>
+    <p>
+      2. Psychotherapeutengesetz<br>
+      Die Regelungen können bei der Psychotherapeutenkammer Rheinland-Pfalz bzw.
+      unter
+      <a href="https://www.gesetze-im-internet.de/psychthg/BJNR131110998.html">
+        https://www.gesetze-im-internet.de/psychthg/BJNR131110998.html
+      </a>
+      eingesehen werden.
+    </p>
+    <h2>Haftungsausschluss</h2>
+    <h3>Haftung für Inhalte</h3>
+    <p>
+      Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die
+      Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch
+      keine Gewähr übernehmen.
+    </p>
+    <p>
+      Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf
+      diesen Seiten nach den allgemeinen Gesetzen verantwortlich.
+    </p>
+    <p>
+      Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht
+      verpflichtet, übermittelte oder gespeicherte fremde Informationen zu
+      überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige
+      Tätigkeit hinweisen.
+    </p>
+    <p>
+      Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen
+      nach den allgemeinen Gesetzen bleiben hiervon unberührt.
+    </p>
+    <p>
+      Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis
+      einer konkreten Rechtsverletzung möglich.
+    </p>
+    <p>
+      Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese
+      Inhalte umgehend entfernen.
+    </p>
+    <h3>Haftung für Links</h3>
+    <p>
+      Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren
+      Inhalte wir keinen Einfluss haben.
+    </p>
+    <p>
+      Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen.
+    </p>
+    <p>
+      Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen.
+    </p>
+    <p>
+      Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder
+       Betreiber der Seiten verantwortlich.
+    </p>
+    <p>
+      Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche
+      Rechtsverstöße überprüft.
+      Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar.
+    </p>
+    <p>
+      Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne
+       konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar.
+    </p>
+    <p>
+      Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links
+      umgehend entfernen.
+    </p>
+    <h3>Urheberrecht</h3>
+    <p>
+      Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen
+      Seiten unterliegen dem deutschen Urheberrecht.
+    </p>
+    <p>
+      Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung
+      außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen
+      Zustimmung des jeweiligen Autors bzw. Erstellers.
+    </p>
+    <p>
+      Downloads und Kopien dieser Seite sind nur für den privaten, nicht
+      kommerziellen Gebrauch gestattet.
+    </p>
+    <p>
+      Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden,
+      werden die Urheberrechte Dritter beachtet.
+    </p>
+    <p>
+      Insbesondere werden Inhalte Dritter als solche gekennzeichnet.
+    </p>
+    <p>
+      Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden,
+      bitten wir um einen entsprechenden Hinweis.
+    </p>
+    <p>
+      Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte
+      umgehend entfernen.
     </p>
   </content-wrapper>
 </template>
