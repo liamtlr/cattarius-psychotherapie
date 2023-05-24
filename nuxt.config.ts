05066b11e541bf5d5d0affaaf33884c5e2987bd6
Liam Taylor <liam@piclo.energy>
2023-05-24 22:06:09 +0100

    remove loaded fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,18 +4,6 @@ export default defineNuxtConfig({
   app: {
     pageTransition: { name: 'fade' },
     head: {
-      link: [
-        {
-          rel: 'stylesheet',
-          href: '//db.onlinewebfonts.com/c/c90524dd000fae4fafbd8f791869cfe1?family=Accent+Graphic',
-          type: 'text/css',
-        },
-        {
-          rel: 'stylesheet',
-          href: '//db.onlinewebfonts.com/c/0ff8990774ec4865c7f45befbafd4910?family=Gotham',
-          type: 'text/css',
-        },
-      ],
       meta: [
         {
           name: 'viewport',
