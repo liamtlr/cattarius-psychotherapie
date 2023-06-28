c8ed0ddbb192adf34276cd352522f5e49fd0b7e3
Liam Taylor <liam@piclo.energy>
2023-06-28 20:34:40 +0100

    add further meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,31 @@ export default defineNuxtConfig({
           content: 'width=device-width, initial-scale=1'
         },
         { name: 'charset', content: 'UTF-8' },
+        {
+          name: "description",
+          content: "Approbierter Psychotherapeut in Mainz, Tiefenpsychologisch fundierte Psychotherapie, Einzeltherapie Mainz, Gruppentherapie Mainz, Paartherapie Mainz, Psychotherapie Marcel Cattarius"
+        },
+        {
+          property: "og:title",
+          content: "Psychotherapie Marcel Cattarius Mainz",
+        },
+        {
+          property: "og:description",
+          content: "Approbierter Psychotherapeut in Mainz, Tiefenpsychologisch fundierte Psychotherapie, Einzeltherapie Mainz, Gruppentherapie Mainz, Paartherapie Mainz, Psychotherapie Marcel Cattarius",
+        },
+        {
+          property: "og:url", content:"https://www.psychotherapiecattarius.de%22/%3E",
+        },
+        {
+          property: "og:site_name", content: "psychotherapiecattarius",
+        },
+        {
+          property: "og:type", content: "website",
+        },
+        {
+          name: "keywords",
+          content: "Psychotherapie Mainz, Einzeltherapie Mainz, Gruppentherapie Mainz, Psychologe Mainz, Psychotherapeut, Psychotherapeutin, Psychotherapie, Paartherapie"
+        },
       ],
       htmlAttrs: {
         lang: 'de',
